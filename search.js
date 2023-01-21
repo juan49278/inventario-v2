70ac8bb9ee7ae105241b652027b097e2709e3e31
juan49278 <juan49278@gmail.com>
2023-01-21 19:22:41 -0300

    Update search.js

--- search.js
+++ search.js
@@ -11,7 +11,7 @@ addEventListener('DOMContentLoaded', async ()=>{
           toAppened += `<tr>
             <th scope="row">${[i + 1]}</th>
             <td class="col-span-2">${data.productos[i].nombre}</td>
-            <td class="col-span-2"><input type="number" placeholder="Cant." class="form-control cant col-8 col-md-3" aria-label="cant"><p class="cant d-none"></p>
+            <td class="col-span-2"><input type="number" placeholder="Cant." class="form-control cant col-8 col-md-3" aria-label="cant" value=${cantidad}><p class="cant d-none"></p>
             <td class="col-span-2"><input type="number" placeholder="$" oninput="calcular()" onchange="guardarLocal()"class="form-control precio col-10 col-md-3" aria-label="precio" value=${parseInt(data.productos[i].Precio)}><p class="precio d-none"></p>
             <td class="col-span-2"> $<span class="total" id="${i}total"></span>
             </tr>`
@@ -22,6 +22,8 @@ addEventListener('DOMContentLoaded', async ()=>{
   const search = document.querySelector('input#search')
   const button = document.getElementById('btnSearch')
   const list = document.querySelector('div#listGroup')
+  let cantidad = document.querySelector("input#quantity")
+  let precio = document.querySelector("input#price")
   
   search.addEventListener('input', ()=>{
     if(search.value !== ""){
@@ -31,42 +33,34 @@ addEventListener('DOMContentLoaded', async ()=>{
     }
   })
   
-  search.addEventListener('keyup', function (event){
-    if(event.code == 'Enter'){
-      filter()
-      $("button").click()
-    }
-  })
+ search.addEventListener("keypress", function(e) {
+      if (e.code === 'Enter') {
+        filter()
+          document.getElementById("btnSearch").click();
+          document.getElementById("btnSearch").onclick = filter();
+      }
+  });
   
   function filter(){
-    listGroup.innerHTML = ""
+    let nombre = ""
     let productos = data.productos
-    let cantidad = document.querySelector("input#quantity")
     for(let producto of productos){
-      if(parseInt(search.value) == producto.codigo){
-        showModal()
-          findResults.innerHTML += `
-        <li class="list-group-item">${producto.nombre} -- Codigo de barra: ${producto.codigo} Cantidad: ${cantidad.innerHTML}</li>
-        `
-        }
-      }
-    }
-  
-  function showModal(){
-    document.getElementById("listGroup").innerHTML = `
+      let nombre = producto.nombre
+      if(search.value == producto.codigo){
+        document.getElementById("listGroup").innerHTML = `
     <form>
   <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Ver Producto</h1>
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Producto: ${nombre}</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
           Ingresar cantidad
-          <input type="number" id="quantity" class="form-control mt-2 mb-2">
+          <input type="number" id="quantity" class="form-control mt-2 mb-2" placeholder="Cantidad">
           Ingresar precio
-          <input type="number" id="price" class="form-control mt-2 mb-2">
+          <input type="number" id="price" class="form-control mt-2 mb-2" placeholder="$">
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Guardar</button>
@@ -75,8 +69,9 @@ addEventListener('DOMContentLoaded', async ()=>{
     </div>
   </div>
   </form>`
-  }
-  function save(){
-    let cantidad = document.querySelector("input#quantity").value
-    let precio = document.querySelector("input#price")
-  }
+        }
+        }
+      }
+  /* function save(){
+    
+  } */
