5894dfc43b47d81c0cbac67a9bd04ed36a30857c
Juan Martinez <110367048+juan49278@users.noreply.github.com>
2023-07-29 19:58:35 -0300

    compact and updated

--- js/search.js
+++ js/search.js
@@ -219,7 +219,16 @@ btnClean.addEventListener('click', () => {
 $(function () {
   $(document).on('click', '.borrar', function (event) {
     event.preventDefault();
-    $(this).closest('tr').remove();
+    swal({
+      title: "¿Estás seguro?",
+    text: "Se borrara el elemento la accion no se puede deshacer",
+    icon: "warning",
+    buttons: true
+  }).then((result) => {
+    if (result == true){
+      $(this).closest('tr').remove();
+    }
+    })
   });
 });
 
@@ -339,9 +348,16 @@ function recoverProductsList() {
     codigo2 = arrays.codigo2
     codigo3 = arrays.codigo3
     codigo4 = arrays.codigo4
-    bodyProduct.innerHTML += `<th scope="row">${nombre}</th>
+    bodyProduct.innerHTML += `<th scope="row">${nombre} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill modificar" viewBox="0 0 16 16">
+    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+  </svg>
+  </th>
             <td class="col-span-2">${precio}</td>
             <td class="col-span-2"><p>${codigo}</p></td>
+            <td class="col-span-2"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="red" class="bi bi-trash borrar" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+          </svg></td>
             <td class="col-span-2 d-none"><p>${codigo1}</p></td>
             <td class="col-span-2 d-none"><p>${codigo2}</p></td>
             <td class="col-span-2 d-none"><p>${codigo3}</p></td>
@@ -349,64 +365,3 @@ function recoverProductsList() {
   }
   modalBody.innerHTML = `${arraysAdded.length}`
 }
-/* 
-function edit(param){
-  console.log(arraysAdded.indexOf(param))
-}
-var current_page = 1;
-var records_per_page = 3
-function prevPage()
-{
-    if (current_page > 1) {
-        current_page--;
-        changePage(current_page);
-    }
-}
-
-function nextPage()
-{
-    if (current_page < numPages()) {
-        current_page++;
-        changePage(current_page);
-    }
-}
-    
-function changePage(page)
-{
-  var elemnts = arraysAdded
-    var btn_next = document.getElementById("btn_next");
-    var btn_prev = document.getElementById("btn_prev");
-    var page_span = document.getElementById("page");
- 
-    // Validate page
-    if (page < 1) page = 1;
-    if (page > numPages()) page = numPages();
-
-    productos.innerHTML = "";
-
-    for (var i = (page-1) * records_per_page; i < (page * records_per_page) && i < elemnts.length; i++) {
-        productos.innerHTML +=  + "<br>";
-    }
-    page_span.innerHTML = page + "/" + numPages();
-
-    if (page == 1) {
-        btn_prev.style.visibility = "hidden";
-    } else {
-        btn_prev.style.visibility = "visible";
-    }
-
-    if (page == numPages()) {
-        btn_next.style.visibility = "hidden";
-    } else {
-        btn_next.style.visibility = "visible";
-    }
-}
-
-function numPages()
-{
-    return Math.ceil(arraysAdded.length / records_per_page);
-}
-
-window.onload = function() {
-    changePage(1);
-}; */
