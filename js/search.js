1e1b943b112cb7934e37c1737e1790c2ee6cbd18
Juan Martinez <110367048+juan49278@users.noreply.github.com>
2023-11-29 21:21:08 -0300

    update

--- js/search.js
+++ js/search.js
@@ -45,10 +45,9 @@ function show() {
             <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
           </svg></td> `
   document.getElementById('productos').innerHTML += toAppened
-  duplicate()
-  new DataTable('#table')
   codeShow = ""
   quantity.value = ""
+  duplicate()
 }
 
 //Mostrar producto agregado a la tabla
@@ -65,12 +64,11 @@ function showAdd() {
             <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
           </svg></td> `
   document.getElementById('productos').innerHTML += toAppened
-  duplicate()
-  new DataTable('#table')
   codeShow = ""
   nombreAdd.value = ""
   quantityNew.value = ""
   priceNew.value = ""
+  duplicate()
 }
 
 //Funcion que despliega div oculto
@@ -348,32 +346,31 @@ function closeModalProducts() {
 //Mostrar Productos actualizados en modal
 function recoverProductsList() {
   for (arrays of arraysAdded) {
+    showSpinner()
     nombre = arrays.nombre
     precio = arrays.Precio
     codigo = arrays.codigo
-    codigo1 = arrays.codigo1
-    codigo2 = arrays.codigo2
-    codigo3 = arrays.codigo3
-    codigo4 = arrays.codigo4
-    bodyProduct.innerHTML += `<th scope="row">${nombre}</th>
+    bodyProduct.innerHTML += `<th scope="row" class="editTdNombre">${nombre}</th>
             <td class="col-span-2">${precio}</td>
             <td class="col-span-2"><p>${codigo}</p></td>
-            <td class="col-span-2" id="${codigo}"><svg xmlns="http://www.w3.org/2000/svg" onclick="selectItem(), idProduct(${codigo})" width="24" height="24" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
-          </svg> <hr>
+            <td class="col-span-2">
           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="red" class="bi bi-trash borrar" viewBox="0 0 16 16">
             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
             <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-          </svg></td>
-            <td class="col-span-2 d-none"><p>${codigo1}</p></td>
-            <td class="col-span-2 d-none"><p>${codigo2}</p></td>
-            <td class="col-span-2 d-none"><p>${codigo3}</p></td>
-            <td class="col-span-2 d-none"><p>${codigo4}</p></td>`
+          </svg></td>`
   }
   modalBody.innerHTML = `${arraysAdded.length}`
-  new DataTable('#bodyProductTable')
+  hideSpinner()
+  let table = new DataTable('#bodyProductTable');
+  table.on('click', '.editTdNombre', function () {
+    let data = table.row(this).data();
+  if (promptNombre == "" || promptNombre == null) {
+    promptNombre = prompt("Ingresa el nombre:", `${data[0]}`);
+  } else {
+    promptNombre = prompt("Ingresa el nombre:", `${promptNombre}`);
+  }
+});
 }
-
 function help() {
   asistant.classList.remove("d-none")
   asistant.classList.add("d-block")
@@ -433,17 +430,6 @@ function idProduct(id) {
   localStorage.setItem('item', id)
 }
 
-function selectItem() {
-  let position = localStorage.getItem('item')
-  elementPrecio = document.getElementById(`${position}`).previousElementSibling.previousElementSibling.innerHTML
-  elementNombre = document.getElementById(`${position}`).previousElementSibling.previousElementSibling.previousElementSibling.innerHTML
-  if (promptNombre == "" || promptNombre == null) {
-    promptNombre = prompt("Ingresa el nombre:", `${elementNombre}`)
-  } else {
-    promptNombre = prompt("Ingresa el nombre:", `${promptNombre}`)
-  }
-}
-
 function selectResult() {
   let position = localStorage.getItem('item')
   element = document.getElementById(`${position}`).innerHTML
@@ -452,3 +438,10 @@ function selectResult() {
   button.onclick = filter();
   searchResult.innerHTML = ""
 }
+/* document.getElementById('submitExport').addEventListener('click', function(e) {
+  e.preventDefault();
+  let export_to_excel = document.getElementById('table');
+  let data_to_send = document.getElementById('data_to_send');
+  data_to_send.value = export_to_excel.outerHTML;
+  document.getElementById('formExport').submit();
+}); */
